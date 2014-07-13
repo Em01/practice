1e015c876df00353526c6facbd7328f95b784576
Emma <emma_williams1@hotmail.co.uk>
2014-07-13 10:56:51 +0100

    conversions

---
+++ conversions.rb
@@ -0,0 +1,10 @@
+#to get the string version of an object we write .to_s
+var1 = 2
+var2 = '5'
+
+puts var1.to_s + var2
+
+#to get the integer version of an object we write to_i
+#to get the float version to_f
+
+#puts will write as a string, the s in puts stands for string
