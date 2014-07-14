548fb175f6368aa12322af10862f85303b72a8ff
Emma <emma_williams1@hotmail.co.uk>
2014-07-14 16:21:29 +0100

    OOP

---
+++ oop.rb
@@ -0,0 +1,16 @@
+#programming languages have procedural, functional and object oriented programming paridgms
+#ruby is an object oriented language with some functional and procedural features
+#OOP is a programming paradigm that uses objects and their interactions to design applications and programs
+#these are the basic OOP concepts
+
+#Abstraction
+#this is the simplifying of complex reality by modeling classes appropriate to the problem
+
+#Polymorphism
+#This is the process of using an operator or function in different ways for different data input
+
+#Encapsulation
+#This hides the implementation details of a class from other objects
+
+#Inheritance
+#This is a way to form new classes using classes that have already been defined.
